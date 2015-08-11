4194dd9a3d2ca6d6ce90c492d70e6141841bf6d9
Torsten Hein <mail@tozzr.com>
2015-08-11 23:24:16 +0200

    from mysqli to pdo with prepared statements to prevent SQL injection
    Signed-off-by: Torsten Hein <mail@tozzr.com>

--- RecipeRepository.php
+++ RecipeRepository.php
@@ -11,58 +11,57 @@ class RecipeRepository extends Repository {
     }
 
     function findRecipe($id) {
-      return $this->findItem(
-        "SELECT id, title, subtitle, authors, text, image " .
-        "FROM recipe " .
-        "WHERE id = " . $id . ";"
-      );
+      return $this->findItemById("recipe", $id);
     }
 
     function moveRecipe($id, $pos) {
-      $res = $this->findItem("SELECT id FROM recipe WHERE position = " . $pos);
-      $otherId = $res['id'];
-      $res = $this->findItem("SELECT position FROM recipe WHERE id = " . $id);
-      $oldPos = $res['position'];
+      $item = $this->findItemById("recipe", $id);
+      $other = $this->findItemByPosition("recipe", $pos);
       $this->query(
-        "UPDATE recipe SET position = " . $pos . " " .
-        "WHERE id = " . $id
+        "UPDATE recipe SET position = :pos WHERE id = :id;",
+        array("pos" => $pos, "id" => $id)
       );
       $this->query(
-        "UPDATE recipe SET position = " . $oldPos . " " .
-        "WHERE id = " . $otherId
+        "UPDATE recipe SET position = :pos WHERE id = :id;",
+        array("pos" => $item['position'], "id" => $other['id'])
       );
     }
 
     function createRecipe($title, $subtitle, $authors, $text, $image) {
-      $res = $this->findItem("SELECT COUNT(id) AS count FROM recipe");
-      $id = $this->create(
+      $res = $this->countItems("recipe");
+      return $this->create(
         "INSERT INTO recipe (title, subtitle, authors, text, image, position) " .
-        "VALUES('" . $title . "', " .
-        "'" . $subtitle . "', " .
-        "'" . $authors . "', ".
-        "'" . $text . "', " .
-        "'" . addslashes($image) . "', " .
-        $res['count'] .
-        ");"
+        "VALUES(:title, :subtitle, :authors, :text, :image, :pos)",
+        array(
+          "title" => $title,
+          "subtitle" => $subtitle,
+          "authors" => $authors,
+          "text" => $text,
+          "image" => addslashes($image),
+          "pos" => $res['count']
+        )
       );
-
-      return $id;
     }
 
     function updateRecipe($id, $title, $subtitle, $authors, $text) {
       $this->query(
-        "UPDATE recipe SET title = '" . $title . "', " .
-        "subtitle = '" . $subtitle . "', " .
-        "authors = '" . $authors . "', " .
-        "text = '" . $text . "' " .
-        "WHERE id = " . $id
+        "UPDATE recipe SET title = :title, " .
+        "subtitle = :subtitle, " .
+        "authors = :authors, " .
+        "text = :text " .
+        "WHERE id = :id;",
+        array(
+          "id" => $id,
+          "title" => $title,
+          "subtitle" => $subtitle,
+          "authors" => $authors,
+          "text" => $text
+        )
       );
     }
 
     function deleteRecipe($id) {
-      $this->query(
-        "DELETE FROM recipe WHERE id = " . $id . ";"
-      );
+      $this->delete("recipe", $id);
     }
 }
 
